22cc66f8b401c9af979b959e27465dda9f3ce034
jmaslanka55 <jmaslank@uwo.ca>
2021-02-17 17:56:02 -0500

    document delete

--- API/document.js
+++ API/document.js
@@ -3,7 +3,7 @@ const Projects = require("../Models/Projects");
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 
-router.get(`/:projectId?`, async (req,res)=>{
+router.get(`/create/:projectId?`, async (req,res)=>{
     try{
         let searchId = req.params.projectId;
         const project = await Projects.findOne({projectId: searchId});
@@ -23,8 +23,25 @@ router.get(`/:projectId?`, async (req,res)=>{
         console.error(err.message);
         res.status(500).send("server error");
     }
-
 });
 
+router.get(`/delete/:projectId/:documentId?`, async (req,res)=>{
+    try{
+        let searchId = req.params.projectId;
+        let docId = req.params.documentId;
+        const project = await Projects.findOne({projectId: searchId});
+        for (let i = 0; i<project.Document.length; i++) {
+            if (project.Document[i].documentId === docId) {
+                project.Document.splice(i, i+1);
+                console.log("Removed");
+            }
+        }
+        await project.save();
+         
+    }catch (err){
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+});
 
 module.exports = router;
--- API/project.js
+++ API/project.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 
 //Get the Project in Mongo that matches the requested ID
-router.get(`/:projectId?`, async (req,res)=>{
+router.get(`/get/:projectId?`, async (req,res)=>{
     try{
         let searchId = req.params.projectId;
         const project = await Projects.findOne({projectId: searchId});
+        console.log()
         res.send("success");
     }catch (err){
         console.error(err.message);
@@ -20,9 +21,9 @@ router.get(`/:projectId?`, async (req,res)=>{
 //Create a new Project with a unique ID (CHANGE FROM GET ONCE DONE TESTING)
 router.get(`/createProject`, (req,res)=>{
     let projectId = uuidv4();
-    let projectName = "Test1"
-    let Duration = "2 weeks"
-    let Description = "Testing description"
+    let projectName = "Test2"
+    let Duration = "3 weeks"
+    let Description = "Testing Poop"
     let project = new Projects({
         projectName,
         Duration,
