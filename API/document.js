e50234fdfd08b7e6cd250234cbf39f2c75fadac1
jmaslanka55 <jmaslank@uwo.ca>
2021-02-17 17:23:00 -0500

    Can add document

--- API/document.js
+++ API/document.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const Projects = require("../Models/Projects");
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 router.get(`/:projectId?`, async (req,res)=>{
     try{
         let searchId = req.params.projectId;
         const project = await Projects.findOne({projectId: searchId});
-        
+        if (!project){
+            res.status(400).send("No project with ID");
+        }
+        project.Document.push(
+            {
+                title: "Test Title",
+                link: "https://docs.google.com/document/d/1LPkwpnhy-18gzczJLjH3bshFYaBXhx68QGZyfUjMt0w/edit#",
+                documentId:  uuidv4()
+            }
+        )
+        await project.save();
+        res.status(200).send("Document Created");   
     }catch (err){
         console.error(err.message);
         res.status(500).send("server error");
     }
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = router;
