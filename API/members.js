6295f0cf1171293e2472771458bdc6df79b2d47a
Jaskanwar <jaskanwar.singh1@gmail.com>
2021-02-17 20:03:27 -0500

    Member endpoints finished

--- API/members.js
+++ API/members.js
@@ -2,5 +2,50 @@ const express = require("express");
 const router = express.Router();
 const Projects = require("../Models/Projects");
 
+//create new member
+router.post("/create", async (req, res) => {
+  try {
+    const { projectId, name, email, phone, github } = req.body;
+    let userId = 1;
+    let project = await Projects.findOne({ projectId });
+    if (!project) {
+      return res.status(400).send("Project does not exist!");
+    }
+
+    project.Member.push({
+      name: name,
+      email: email,
+      phone: phone,
+      userId: userId,
+      github: github,
+    });
+
+    await project.save();
+
+    return res.status(200).send("Member was added succesfully!");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
+//delete member
+router.post("/delete", async (req, res) => {
+    try {
+      const { projectId, userId } = req.body;
+      let project = await Projects.findOne({ projectId });
+      for (let i = 0; i < project.Member.length; i++) {
+        if (project.Member[i].userId === userId) {
+          project.Member.splice(i, i + 1);
+        }
+      }
+      await project.save();
+  
+      return res.status(200).send("Member was deleted!");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
 
 module.exports = router;
